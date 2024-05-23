211d807b573f42d3661dc48392f338e6df5bdc98
FixGFATHER <drdonutdani@gmail.com>
2024-05-22 22:27:05 -0700

    Add files via upload

---
+++ tools/upx.py
@@ -0,0 +1,38 @@
+import os
+import platform
+import requests
+import shutil
+import zipfile
+
+
+class UPX():
+    def __init__(self):
+        self.version = "4.2.4"
+        self.url = f"https://github.com/upx/upx/releases/download/v{self.version}/upx-{self.version}-win{platform.architecture()[0][:2]}.zip"
+
+        self.check()
+        self.download()
+        self.extract()
+        self.cleanup()
+
+    def check(self):
+        if os.path.exists("./tools/upx.exe"):
+            os.remove("./tools/upx.exe")
+
+    def download(self):
+        response = requests.get(self.url)
+        with open("upx.zip", "wb") as f:
+            f.write(response.content)
+
+    def extract(self):
+        with zipfile.ZipFile("upx.zip") as zip_file:
+            zip_file.extractall()
+            shutil.move(f"./upx-{self.version}-win{platform.architecture()[0][:2]}/upx.exe", "./tools")
+
+    def cleanup(self):
+        os.remove("upx.zip")
+        shutil.rmtree(f"upx-{self.version}-win{platform.architecture()[0][:2]}")
+
+
+if __name__ == "__main__":
+    UPX()
